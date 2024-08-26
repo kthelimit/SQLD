83e11c81589425930c0742adf2c18ffdef0233c7
K <73793167+kthelimit@users.noreply.github.com>
2024-08-26 13:52:23 +0900

    SQLD 240807~240822
    SQLD 공부한 코드들(txt로 백업해뒀다)

---
+++ 240820/0820_1.sql
@@ -0,0 +1,109 @@
+-- 현장 노하우
+
+-- 1. 이탈리아의 연도, 사원별, 매출액 계산
+SELECT SUBSTR(a.sales_month, 1, 4) as years,
+        a.employee_id, 
+        SUM(a.amount_sold) AS amount_sold
+   FROM sales a,
+        customers b,
+        countries c
+  WHERE a.cust_id = b.CUST_ID
+    AND b.country_id = c.COUNTRY_ID
+    AND c.country_name = 'Italy'     
+  GROUP BY SUBSTR(a.sales_month, 1, 4), a.employee_id;
+  
+--2. 연도별 최대 매출액 추출
+SELECT  years,  MAX(amount_sold) AS max_sold
+  FROM (  SELECT SUBSTR(a.sales_month, 1, 4) as years,
+                        a.employee_id, 
+                        SUM(a.amount_sold) AS amount_sold
+                   FROM sales a,
+                        customers b,
+                        countries c
+                  WHERE a.cust_id = b.CUST_ID
+                    AND b.country_id = c.COUNTRY_ID
+                    AND c.country_name = 'Italy'     
+                  GROUP BY SUBSTR(a.sales_month, 1, 4), a.employee_id) k
+  GROUP BY years
+  ORDER BY years;
+
+-- 3. 1과 2의 결과를 조인해서 최대 매출을 일으킨 사원을 추출
+SELECT emp.years, 
+       emp.employee_id,
+       emp.amount_sold
+  FROM ( SELECT SUBSTR(a.sales_month, 1, 4) as years,
+                a.employee_id, 
+                SUM(a.amount_sold) AS amount_sold
+           FROM sales a,
+                customers b,
+                countries c
+          WHERE a.cust_id = b.CUST_ID
+            AND b.country_id = c.COUNTRY_ID
+            AND c.country_name = 'Italy'     
+          GROUP BY SUBSTR(a.sales_month, 1, 4), a.employee_id   
+        ) emp,
+       ( SELECT  years, 
+                 MAX(amount_sold) AS max_sold
+          FROM ( SELECT SUBSTR(a.sales_month, 1, 4) as years,
+                        a.employee_id, 
+                        SUM(a.amount_sold) AS amount_sold
+                   FROM sales a,
+                        customers b,
+                        countries c
+                  WHERE a.cust_id = b.CUST_ID
+                    AND b.country_id = c.COUNTRY_ID
+                    AND c.country_name = 'Italy'     
+                  GROUP BY SUBSTR(a.sales_month, 1, 4), a.employee_id    
+               )
+          GROUP BY years
+       ) sale
+  WHERE emp.years = sale.years
+    AND emp.amount_sold = sale.max_sold 
+  ORDER BY years;
+
+-- 4. 3의 결과와 사원테이블을 조인해서 사원이름을 추출
+SELECT emp.years, 
+       emp.employee_id,
+       emp2.emp_name,
+       emp.amount_sold
+  FROM ( SELECT SUBSTR(a.sales_month, 1, 4) as years,
+                a.employee_id, 
+                SUM(a.amount_sold) AS amount_sold
+           FROM sales a,
+                customers b,
+                countries c
+          WHERE a.cust_id = b.CUST_ID
+            AND b.country_id = c.COUNTRY_ID
+            AND c.country_name = 'Italy'     
+          GROUP BY SUBSTR(a.sales_month, 1, 4), a.employee_id   
+        ) emp,
+       ( SELECT  years, 
+                 MAX(amount_sold) AS max_sold
+          FROM ( SELECT SUBSTR(a.sales_month, 1, 4) as years,
+                        a.employee_id, 
+                        SUM(a.amount_sold) AS amount_sold
+                   FROM sales a,
+                        customers b,
+                        countries c
+                  WHERE a.cust_id = b.CUST_ID
+                    AND b.country_id = c.COUNTRY_ID
+                    AND c.country_name = 'Italy'     
+                  GROUP BY SUBSTR(a.sales_month, 1, 4), a.employee_id    
+               )
+          GROUP BY years
+       ) sale,
+       employees emp2
+  WHERE emp.years = sale.years
+    AND emp.amount_sold = sale.max_sold 
+    AND emp.employee_id = emp2.employee_id
+ORDER BY years;
+
+
+
+
+
+
+
+
+
+
